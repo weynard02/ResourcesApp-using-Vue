f06de0305e4a76cb521dc390f6538e8400f5b40c
weynard02 <samsicoweynard@gmail.com>
2023-08-23 14:26:04 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Resources App using Vue.js
+
+How to use:
+
+```
+npm install
+npm run serve
+```
+
+![image](image.png)
---
+++ src/components/resources/StoredResources.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <item-resource
+      v-for="res in resources"
+      :key="res.id"
+      :id="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></item-resource>
+  </ul>
+</template>
+
+<script>
+import ItemResource from './ItemResource.vue';
+
+export default {
+  inject: ['resources'],
+  components: {
+    ItemResource,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
